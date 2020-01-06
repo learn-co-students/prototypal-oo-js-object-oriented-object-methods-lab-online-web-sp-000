4abc664450d6cf285d2267c5b74834d567de29de
stabassum <sumu1993@gmail.com>
2020-01-05 23:15:35 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code below
+
+    function BoardMember(name, homeState, training){
+        this.name = name;
+        this.homeState = homeState;
+        this.training = training;
+    }
+
+    BoardMember.prototype.veto() = function(){
+        return "I must agree";
+    }
+
+    BoardMember.prototype.approve() = function(){
+        return "You can do that!";
+    }
+
+    BoardMember.prototype.doCharity() = function(){
+
+    }
+
+
