d96e1802374c1418fef584539f3eadce9194ed37
carrollm2 <carrollm2@gmail.com>
2020-10-27 20:15:59 -0400

    Done.

--- index.js
+++ index.js
@@ -21,5 +21,5 @@ function BoardMember(name, homeState, training) {
   };
 
   BoardMember.prototype.saysHi = function() {
-    return `Hi, my name is ${this.name}. I am from ${this.homestate}, and I was trained in ${this.training}.`
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
   };
