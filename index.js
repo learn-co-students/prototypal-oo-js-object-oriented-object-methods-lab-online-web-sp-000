808a22de29a51ea1f6b1616d472f1e1716eff8a7
cantstopcoding <jonathan_koss@yahoo.com>
2020-11-07 10:49:01 -0500

    Done.

--- index.js
+++ index.js
@@ -3,23 +3,23 @@ function BoardMember(name, homeState, training) {
     this.homeState = homeState;
     this.training = training;
 
-    BoardMember.veto = function() {
+    BoardMember.prototype.veto = function() {
         return "No, I must disagree";
     };
 
-    BoardMember.approve = function() {
+    BoardMember.prototype.approve = function() {
         return "You can do that!";
     }
 
-    BoardMember.doCharity = function() {
+    BoardMember.prototype.doCharity = function() {
         return "I like to help people.";l
     }
 
-    BoardMember.releasePressStatement = function() {
+    BoardMember.prototype.releasePressStatement = function() {
         return "You will see great things from Scuber."
     }
 
-    BoardMember.sayHi = function() {
+    BoardMember.prototype.sayHi = function() {
         return `Hi, my name is ${this.name}. I am from New York, and I was trained in law.`
     }    
 }
