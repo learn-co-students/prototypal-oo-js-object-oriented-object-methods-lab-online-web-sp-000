9fb50be1c2cf8c9fe91f8351def4e68831c3ff10
ashleymader <ashley.mader12@gmail.com>
2021-01-26 09:58:14 -0500

    Done.

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
-class BoardMember { 
-    constructor(name, homeState, training) {
+function BoardMember(name, homeState, training) {
         this.name = name 
         this.homeState = homeState 
         this.training = training
     }
 
-    veto() { 
-        return `No, I must disagree`
+    BoardMember.prototype.veto = function() { 
+        return 'No, I must disagree'
     }
 
-    approve() { 
-        return `You can do that!`
+    BoardMember.prototype.approve = function() { 
+        return 'You can do that!'
     }
 
-    doCharity() { 
-        return `I like to help people.`
+    BoardMember.prototype.doCharity = function() {
+        return 'I like to help people.'
     }
 
-    releasePressStatement() { 
-        return `You will see great things from Scuber`
+    BoardMember.prototype.releasePressStatement = function() { 
+        return 'You will see great things from Scuber.'
     }
-}
 
-BoardMember.prototype.sayHi = function(name, homeState, training) { 
-    return `Hi, my name is ${name}. I am from ${homeState}, and I was trained in ${training}.`
-}
+    BoardMember.prototype.sayHi = function() { 
+        return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+    }
+
+
