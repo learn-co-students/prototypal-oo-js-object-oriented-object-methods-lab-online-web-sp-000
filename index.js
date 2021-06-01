c7fa65e0d789f6a59a548eec159f102fd84c7ac2
dbrandt1990 <dvbrandt90@gmail.com>
2021-05-31 18:24:50 -0600

    Done.

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ function BoardMember(name, homeState, training) {
     this.name = name;
     this.homeState = homeState;
     this.training = training;
-    this.veto = () => "No, I must disagree"
-    this.approve = () => "You can do that!"
-    this.doCharity = () => "I like to help people."
-    this.releasePressStatement = () => "You will see great things from Scuber."
-    this.sayHi = () => `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+    BoardMember.prototype.veto = () => "No, I must disagree"
+    BoardMember.prototype.approve = () => "You can do that!"
+    BoardMember.prototype.doCharity = () => "I like to help people."
+    BoardMember.prototype.releasePressStatement = () => "You will see great things from Scuber."
+    BoardMember.prototype.sayHi = () => `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
 }
